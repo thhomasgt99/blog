761891aae43805509940eb55823f04831dc073b0
Thomas Bernal <thhomasgt99@gmail.com>
2024-07-01 17:22:39 -0500

    13 commit

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,9 +1,47 @@
+const User = require('../models/userModel')
+
 function prueba(req, res) {
 	res.status(200).send({
-		message: 'hola mundo desde Node'
+		message: 'hola Thomi'
 	})
+}
+
+function saveUser(req, res) {
+	var params = req.body
+	const newUser = new User()
+
+	if (params.name && params.social_profile  && params.rating && params.last_login) {
+		newUser.name = params.name
+		newUser.user_role = params.user_role
+		newUser.status = params.status
+		newUser.social_profile = params.social_profile
+		newUser.promote = params.promote
+		newUser.rating = params.rating
+		newUser.last_login = params.last_login
+	}
+
+	User.find()
+		.then((usuarios) => {
+			const filter = usuarios.some(usuario => usuario.name == newUser.name)
+			if (filter) {
+				res.status(404).send({message: 'el nombre de usuario ya existe'})
+			} else {
+				newUser.save().then((item) => {
+					res.status(200).send(item)
+				}).catch((err) => {
+					console.error('Error al guardar el usuario', err)
+				})
+			}
+		})
+		.catch((err) => {
+			console.error('Error al obtener la lista de usuarios:', err);
+		});
+
+		return newUser
+
 }
 
 module.exports = {
 	prueba,
+	saveUser,
 }
--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 // Importar Express
 const app = require('./app')
+const mongoose = require('mongoose')
 
 // Configurar el servidor para que escuche en el puerto 3000
 const PORT = process.env.PORT || 3800;
-app.listen(PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Servidor escuchando en el puerto ${PORT}`);
+// });
+
+//conexion database
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/blog')
+	.then(()=>{
+		console.log("La conexion a la base de datos se ha realizado correctamente")
+
+		//crear servidor
+		app.listen(PORT, ()=>{
+			console.log("Servidor corriendo en http://localhost:3800")
+		})
+
+	})
+	.catch(err => console.log(err))
