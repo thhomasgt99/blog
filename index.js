3f2fba9cd23c8567df4468e564212ffb8d5d705a
Thomas Bernal <thhomasgt99@gmail.com>
2024-07-01 17:50:21 -0500

    18 commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Importar Express
 const app = require('./app')
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
 // Configurar el servidor para que escuche en el puerto 3000
 const PORT = process.env.PORT || 3800;
@@ -9,16 +9,16 @@ const PORT = process.env.PORT || 3800;
 // });
 
 //conexion database
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://localhost:27017/blog')
-mongoose.connect('mongodb+srv://thhomasgt99:lPSWgHFfHqt7fkGu@cluster0.tykp5qz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-	.then(()=>{
-		console.log("La conexion a la base de datos se ha realizado correctamente")
+// mongoose.connect('mongodb+srv://thhomasgt99:lPSWgHFfHqt7fkGu@cluster0.tykp5qz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+// 	.then(()=>{
+// 		console.log("La conexion a la base de datos se ha realizado correctamente")
 
-		//crear servidor
+// 		//crear servidor
 		app.listen(PORT, ()=>{
 			console.log("Servidor corriendo en http://localhost:3800")
 		})
 
-	})
-	.catch(err => console.log(err))
+// 	})
+// 	.catch(err => console.log(err))
