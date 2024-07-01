661f0a6573ab44b7b598f910bce50fa7beed72e7
Thomas Bernal <thhomasgt99@gmail.com>
2024-07-01 18:11:15 -0500

    20 commit

--- index.js
+++ index.js
@@ -9,16 +9,20 @@ const PORT = process.env.PORT || 3800;
 // });
 
 //conexion database
-mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://localhost:27017/blog')
-mongoose.connect('mongodb+srv://thhomasgt99:lPSWgHFfHqt7fkGu@cluster0.tykp5qz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-	.then(()=>{
-		console.log("La conexion a la base de datos se ha realizado correctamente")
+async function arbi() {
+    mongoose.Promise = global.Promise;
+    // mongoose.connect('mongodb://localhost:27017/blog')
+    await mongoose.connect('mongodb+srv://thhomasgt99:lPSWgHFfHqt7fkGu@cluster0.tykp5qz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+        .then(() => {
+            console.log("La conexion a la base de datos se ha realizado correctamente")
 
-		//crear servidor
-		app.listen(PORT, ()=>{
-			console.log("Servidor corriendo en http://localhost:3800")
-		})
+            //crear servidor
+            app.listen(PORT, () => {
+                console.log("Servidor corriendo en http://localhost:3800")
+            })
 
-	})
-	.catch(err => console.log(err))
+        })
+        .catch(err => console.log(err))
+}
+
+arbi()
