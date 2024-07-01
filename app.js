e2bb434d4506226fd1c04030027e72404a2908ac
Thomas Bernal <thhomasgt99@gmail.com>
2024-06-30 20:36:54 -0500

    8 commit

--- app.js
+++ app.js
@@ -1,39 +1,8 @@
-'use strict'
+const express = require('express');
+const app = express();
 
-var express = require('express')
-// var bodyParser = require('body-parser')
-// var routes = require('./routes/routes')
-var app = express()
+app.get('/', (req, res) => {
+	res.send('¡Hola, mundo!');
+});
 
-var express = require('express')
-
-var api = express.Router()
-
-function prueba(req, res) {
-	res.status(200).send({
-		message: 'hola mundo desde Node'
-	})
-}
-
-api.get('/', prueba)
-
-//cargar rutas
-// var user_routes = require('./routes/userRoute')
-
-const cors = require('cors');
-
-
-// //middlewares
-// app.use(bodyParser.urlencoded({extended:false}))
-// app.use(bodyParser.json())
-
-
-//cors
-app.use(cors());
-
-//rutas
-// app.use('/api', ()=>{
-// app.use('/', routes)
-
-//exportar
 module.exports = app
--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 // Importar Express
-const express = require('express');
-
-// Crear una instancia de la aplicación Express
-const app = express();
-
-// Definir la ruta GET
-app.get('/', (req, res) => {
-    res.send('¡Hola, mundo!');
-});
+const app = require('./app')
 
 // Configurar el servidor para que escuche en el puerto 3000
 const PORT = process.env.PORT || 3800;
