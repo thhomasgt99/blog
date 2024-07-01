9b70a72a1a4cccc105cd4970fc6b7963629db694
Thomas Bernal <thhomasgt99@gmail.com>
2024-07-01 16:32:40 -0500

    10 commit

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+
 const app = express();
+
+//middlewares
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+
+app.use(cors());
+
 
 app.get('/', (req, res) => {
 	res.send('¡Hola desde app!');
