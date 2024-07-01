d24a53fe936c619f949a77f8044447c5e216ba12
Thomas Bernal <thhomasgt99@gmail.com>
2024-07-01 17:44:04 -0500

    16 commit

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const PORT = process.env.PORT || 3800;
 
 //conexion database
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/blog')
+// mongoose.connect('mongodb://localhost:27017/blog')
+mongoose.connect('mongodb+srv://thhomasgt99:lPSWgHFfHqt7fkGu@cluster0.tykp5qz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 	.then(()=>{
 		console.log("La conexion a la base de datos se ha realizado correctamente")
 
--- models/userModel.js
+++ models/userModel.js
@@ -1,15 +1,15 @@
-// var mongoose = require('mongoose')
-// var Schema = mongoose.Schema
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
 
-// var	UserSchema = Schema({
-// 	name:String,
-// 	user_role:String,
-// 	status:Boolean,
-// 	social_profile:Array,
-// 	promote:Boolean,
-// 	rating:Number,
-// 	last_login:String,
-// 	created_at:String,
-// })
+var	UserSchema = Schema({
+	name:String,
+	user_role:String,
+	status:Boolean,
+	social_profile:Array,
+	promote:Boolean,
+	rating:Number,
+	last_login:String,
+	created_at:String,
+})
 
-// module.exports = mongoose.model('User', UserSchema)
+module.exports = mongoose.model('User', UserSchema)
